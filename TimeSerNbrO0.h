3cdd38afe225e6421f11f375a9150ffed8e7fe7c
Robert Millward <robertmillward@yahoo.com>
2020-02-01 21:24:24 -0800

    950 TimeSerNbrLink

--- TimeSerNbrO0.h
+++ TimeSerNbrO0.h
@@ -38,6 +38,26 @@ typedef const struct TimeSerNbrO0HelperClassStructure
 #define TimeSerNbrHCapi TimeSerNbrO0HCapi
 extern TimeSerNbrHCapiT TimeSerNbrHCapi;
 
+typedef struct TimeSerNbrLinkO0HelperInstanceApiStruct
+{
+    void (*saveRow)(TimeSerNbrLinkZ0HIdataPT);
+    void (*trashRow)(TimeSerNbrLinkZ0HIdataPT);
+    void (*toRow)(TimeSerNbrLinkZ0HIdataPT);
+}TimeSerNbrLinkO0HIapiT, *TimeSerNbrLinkO0HIapiPT;
+extern TimeSerNbrLinkO0HIapiT TimeSerNbrLinkO0HIapi;
+
+typedef struct TimeSerNbrLinkZ0HelperInstanceNewStruct
+{
+    TimeSerNbrLinkO0HIapiPT apiP;
+    TimeSerNbrLinkZ0HIdataT data;
+}TimeSerNbrLinkO0HInewT, *TimeSerNbrLinkO0HInewPT;
+
+typedef struct TimeSerNbrLinkZ0HelperClassApiStruct
+{
+    
+}TimeSerNbrLinkO0HCapiT, *TimeSerNbrLinkO0HCapiPT;
+extern TimeSerNbrLinkO0HCapiT TimeSerNbrLinkO0HCapi;
+
 #endif /* defined(TimeSerNbrO0_h) */
 /**
  */
