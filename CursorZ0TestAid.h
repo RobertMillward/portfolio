bc5b866375c1dbc2c4b6b9ff9cc567eed7996df1
Robert Millward <robertmillward@yahoo.com>
2020-02-06 15:25:59 -0800

    Upload Cursor

---
+++ CursorZ0TestAid.h
@@ -0,0 +1,96 @@
+//
+//  CursorZ0TestAid.h
+//
+//  Created by Robert Russell Millward on 10/4/13.
+//  Copyright (c) 2013 Robert Russell Millward.
+//
+
+#ifndef CursorZ0TestAid_h
+#define CursorZ0TestAid_h
+
+#include "CursorZ0Plan.h"
+#include "CursorZ0.h"
+#include "RowZ0Plan.h"
+#include "CompareZ0.h"
+
+
+typedef struct CursorZ0QIapiS
+{
+    /**
+     * Create a ready to journal string.
+     */
+    void (*toRow)(CursorZ0AIdataPT data,
+                    char * here,
+                  lineNbrT);
+    /**
+     * Get a pointer to the designated field.
+     */
+    char* (*getField)(CursorZ0AIdataPT data,
+                      fieldLetterCursorPT field,
+                      lineNbrT);
+    /**
+     * Update to/from without deleting missing from
+     * fields.
+     *
+     * The field identifiers must be related.
+     */
+    // For sure ROW_ITEM_NAME must match.
+    // Probably ROW_ID should match.
+    void (*updateMatchingCursor)(CursorZ0AIdataPT data,
+                              CursorZ0AIdataPT that,
+                                 lineNbrT);
+    /**
+     * Clear or drop the value for the designated field.
+     */
+    void (*dropField)(CursorZ0AIdataPT data,
+                      fieldLetterCursorPT field,
+                      lineNbrT);
+    /**
+     * Compare per the sortOn list
+     * (see CPR_FTYPE_*).
+     * Example for
+     * - Nulls High,
+     * - field a, left justified string,
+     * - field b, number,
+     * - field c, date-time
+     * sortOn literally looks like "Halbnct"
+     */
+    char *(*sort)(CursorZ0AIdataPT data,
+                  CursorZ0AIdataPT that,
+                  sortCtlCprPT sortOnP,
+                  lineNbrT);
+    /**
+     * Tools
+     */
+    
+    
+    /**
+     * Tests
+     */
+    ErrorWarnCountT (*toRowSafety)(lineNbrT, cfuncNameT);
+    
+}CursorZ0QIapiT, *CursorZ0QIapiPT;
+
+extern CursorZ0QIapiT CursorZ0QIapi;
+
+typedef struct CursorZ0QInewS
+{
+    CursorZ0AIdataT data;
+    
+    // Instance functions:
+    CursorZ0QIapiPT api;  // The externalized methods to keep the instance as small as possible.
+    
+} CursorZ0QInewT, *CursorZ0QInewPT;
+
+
+
+/**
+ * A newCursor reformats the input data (journal or commaSep)
+ * so preserve the input row first as necessary.
+ */
+extern CursorZ0QInewT newCursorZ0Test(char *jnlOrCsvLine, gpTxPplGrpBatchTwoWayZ0PT, lineNbrT);
+
+
+
+
+#endif /* CursorZ0TestAid_h */
---
+++ CursorZ0XCTests.m
@@ -0,0 +1,231 @@
+//
+//  CursorZ0Tests.m
+//
+//  Created by Robert Russell Millward on 10/8/13.
+//  Copyright (c) 2013 Robert Russell Millward.
+//  All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+//#import "CursorZ0.h"
+//#import "TransactionZ0.h"
+#import "TestAidZ0.h"
+//#import "ArchitectureZ0.h"
+//#import "InitDestroyHelperZ0.h"
+//#import "PeopleGroupO2.h"
+#import "CursorZ0TestAid.h"
+
+ 
+
+@interface CursorZ0NewTests : XCTestCase
+
+@end
+
+@implementation CursorZ0NewTests
+
+
+- (void)setUp
+{
+    TestAidC.nextTx();
+}
+
+- (void)tearDown
+{
+    TestAidC.getCounts();
+}
+
+
+#pragma mark - Tests
+
+
+- (void)test2019NewEmptyZ0{
+    if(TestAidC.isScheduled(__FUNCTION__, TestAidZ0QCdata.showFunctionName)){
+
+        ErrorWarnCountT errWrn = CursorZ0QIapi.toRowSafety(__LINE__, __FUNCTION__);
+        
+        if(errWrn.instanceErrors != 0){
+            XCTAssert(NO, @"%@", [[NSString alloc] initWithUTF8String:TestAidC.getAssertText(__FUNCTION__)]);
+        }
+    }
+}
+
+
+
+
+/** EOW
+- (void)test2019newCursor0EOWZ0{
+    if(TestAidC.isScheduled(__FUNCTION__, TestAidZ0QCdata.showFunctionName)){
+        char *doCursorStr = "item\034wrowID\034x1\034ysome data\034a" EOW;
+        char wkCursorStr[1001];
+        char ckCursorStr[1001];
+        strcpy(wkCursorStr, doCursorStr);
+        CursorZ0AInewT tR = newCursor(wkCursorStr, &TestAidZ0QCdata.twoWay);
+        tR.api->toRow(&tR.data, ckCursorStr);
+        
+        strcat(ckCursorStr, EOW);
+        if(TestAidC.putTestChars(doCursorStr, ckCursorStr, __LINE__) != 0){
+            XCTAssert(NO, @"%@", [[NSString alloc] initWithUTF8String:TestAidC.getAssertText(__FUNCTION__)]);
+        }
+    }
+}
+ */
+
+
+- (void)test2019newCursor0NoEOWZ0{
+    if(TestAidC.isScheduled(__FUNCTION__, TestAidZ0QCdata.showFunctionName)){
+        char *doCursorStr = "item\034wrowID\034x1\034ysome data\034a";
+        char wkCursorStr[1001];
+        char ckCursorStr[1001];
+        strcpy(wkCursorStr, doCursorStr);
+        CursorZ0AInewT tR = newCursor(wkCursorStr, &TestAidZ0QCdata.twoWay);
+        tR.api->toRow(&tR.data, ckCursorStr);
+        
+        if(TestAidC.putTestChars(doCursorStr, ckCursorStr, __LINE__) != 0){
+            XCTAssert(NO, @"%@", [[NSString alloc] initWithUTF8String:TestAidC.getAssertText(__FUNCTION__)]);
+        }
+    }
+}
+
+- (void)test2019newCursorZ0Csv{
+    if(TestAidC.isScheduled(__FUNCTION__, TestAidZ0QCdata.showFunctionName)){
+        char *doCursorStr = "zaz,\"zbz\",zcz,,zez,zfz,,,";
+        char *ckCursorStr = "zaz" FSS_ARC "a" "zbz" FSS_ARC "b" "zcz" FSS_ARC "c" "zez" FSS_ARC "e" "zfz" FSS_ARC "f";
+        char wkcsvCursorStr[1001];
+        strcpy(wkcsvCursorStr, doCursorStr);
+        CursorZ0AInewT tR = newCursor(wkcsvCursorStr, &TestAidZ0QCdata.twoWay);
+        char wkjnlCursorStr[1001];
+        tR.api->toRow(&tR.data, wkjnlCursorStr);
+        
+        if(TestAidC.putTestChars(ckCursorStr, wkjnlCursorStr, __LINE__) != 0)
+        {
+            XCTAssert(NO, @"%@", [[NSString alloc] initWithUTF8String:TestAidC.getAssertText(__FUNCTION__)]);
+        }
+    }
+}
+@end
+
+
+@interface CursorZ0SizeTests : XCTestCase
+
+@end
+
+@implementation CursorZ0SizeTests
+
+
+- (void)setUp
+{
+    TestAidC.nextTx();
+}
+
+- (void)tearDown
+{
+    TestAidC.getCounts();
+}
+
+- (void)test2999SizeofCursorZ0{
+    // Added functionality and bigger x-field in early 2015.
+    if(TestAidC.putTestInts(240, sizeof(CursorZ0AInewT), __LINE__) != 0){
+        XCTAssert(NO, @"%@", [[NSString alloc] initWithUTF8String:TestAidC.getAssertText(__FUNCTION__)]);
+    }
+}
+
+
+@end
+
+
+
+@interface CursorZ0OtherTests : XCTestCase
+
+@end
+
+@implementation CursorZ0OtherTests
+
+- (void)test2019SortLowEqlHighZ0{
+    if(TestAidC.isScheduled(__FUNCTION__, TestAidZ0QCdata.showFunctionName)){
+        char *rowAsrc = "item" FSS_ARC "w13" FSS_ARC "x1" FSS_ARC "ysome data" FSS_ARC "a";
+        char *rowBsrc = "item" FSS_ARC "w13" FSS_ARC "x2" FSS_ARC "ymore data" FSS_ARC "a";
+        char rowAchars[1001];
+        char rowBchars[1001];
+        char* sortInfoP = "HaS";
+        
+        strcpy(rowAchars, rowAsrc);
+        CursorZ0AInewT rowA = newCursor(rowAchars, &TestAidZ0QCdata.twoWay);
+        if(TestAidC.putTestChars(IS_EQL_ARC,
+                                  rowA.api->sort(&rowA.data, &rowA.data, sortInfoP),
+                                  __LINE__) != 0 ||
+           TestAidC.putTestChars("some data",
+                                  rowA.api->getField(&rowA.data, &sortInfoP[1]),
+                                  __LINE__) != 0 ||
+           1 == 2){
+            XCTAssert(NO, @"%@", [[NSString alloc] initWithUTF8String:TestAidC.getAssertText(__FUNCTION__)]);
+        }
+        
+        
+        strcpy(rowBchars, rowBsrc);
+        CursorZ0AInewT rowB = newCursor(rowBchars, &TestAidZ0QCdata.twoWay);
+        
+        if(TestAidC.putTestChars(IS_HIGH_ARC,
+                                  rowA.api->sort(&rowA.data, &rowB.data, sortInfoP),
+                                  __LINE__) != 0 ||
+           TestAidC.putTestChars("some data",
+                                  rowA.api->getField(&rowA.data, &sortInfoP[1]),
+                                  __LINE__) != 0 ||
+           1 == 2){
+            XCTAssert(NO, @"%@", [[NSString alloc] initWithUTF8String:TestAidC.getAssertText(__FUNCTION__)]);
+        }
+        
+        
+        if(TestAidC.putTestChars(IS_LOW_ARC,
+                                  rowA.api->sort(&rowB.data, &rowA.data, sortInfoP),
+                                  __LINE__) != 0 ||
+           TestAidC.putTestChars("more data",
+                                  rowB.api->getField(&rowB.data, &sortInfoP[1]),
+                                  __LINE__) != 0 ||
+           1 == 2){
+            XCTAssert(NO, @"%@", [[NSString alloc] initWithUTF8String:TestAidC.getAssertText(__FUNCTION__)]);
+        }
+    }
+}
+
+
+- (void) test2019UpdateMatchingCursorZ0{
+    if(TestAidC.isScheduled(__FUNCTION__, TestAidZ0QCdata.showFunctionName)){
+        char *origCursorStr = "item" FSS_ARC "w13" FSS_ARC "x1" FSS_ARC "ysome data" FSS_ARC "a";
+        char *updtCursorStr = "item" FSS_ARC "w13" FSS_ARC "x2" FSS_ARC "ymore data" FSS_ARC "b";
+        char wkoCursorStr[1001];
+        char wkuCursorStr[1001];
+        char *myGotChars;
+        char *myExpectChars;
+        
+        strcpy(wkoCursorStr, origCursorStr);
+        CursorZ0AInewT tRo = newCursor(wkoCursorStr, &TestAidZ0QCdata.twoWay);
+        
+        strcpy(wkuCursorStr, updtCursorStr);
+        CursorZ0AInewT tRu = newCursor(wkuCursorStr, &TestAidZ0QCdata.twoWay);
+        
+        tRo.api->updateMatchingCursor(&tRo.data, &tRu.data);
+        
+        for(char fIX = 'a' ; fIX <= 'z' ; fIX++){
+            
+            myGotChars = tRo.api->getField(&tRo.data, &fIX);
+            
+            switch (fIX) {
+                case ITEM_NAME_ROW: myExpectChars = "item";         break;
+                case ID_ROW:        myExpectChars = "13";           break;
+                case STATUS_ROW:    myExpectChars = "2";            break;
+                case SOURCE_ROW:    myExpectChars = "";             break;
+                case 'a':           myExpectChars = "some data";    break;
+                case 'b':           myExpectChars = "more data";    break;
+                default:            myExpectChars = NULL;           break;
+            }
+            
+            if(TestAidC.putTestChars(myExpectChars, myGotChars, __LINE__) != 0){
+                XCTAssert(NO, @"%@", [[NSString alloc] initWithUTF8String:TestAidC.getAssertText(__FUNCTION__)]);
+            }
+        }
+    }
+}
+
+@end
+
+//END CursorZ0Tests.m
