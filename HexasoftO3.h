095f3a2605e3db7761dbbdc95cd18d617875ddd8
Robert Millward <robertmillward@yahoo.com>
2020-03-03 13:49:20 -0800

    950 converting Hexasoft

--- HexasoftO3.h
+++ HexasoftO3.h
@@ -1,45 +1,42 @@
 //
 //  HexasoftO3.h
+//  2020Mar10Dev
 //
 //  Created by Robert R on 2/18/2019.
-//  Copyright (c) 2019 Robert Russell Millward.
-//  All rights reserved.
+//  Copyright (c) 2019 Robert Russell Millward.  All rights reserved.
 //
-/**
- *
- */
-#ifndef __HexasoftO3__
-#define __HexasoftO3__
-#include "InitDestroyHelperO0.h"
+#ifndef HexasoftO3_h
+#define HexasoftO3_h
+//#include "InitDestroyHelperO0.h"
 #include "CommaSepO3.h"
 
-typedef struct HexasoftO3toInstanceDataStruct // to for data
+typedef struct HexasoftO3ServiceInstanceDataStruct // to for data
 {
     // Not used, no additional data required
-}HexasoftO3toIT, *HexasoftO3toIPT;
+}HexasoftO3SIdataT, *HexasoftO3SIdataPT;
 
-typedef struct HexasoftO3goInstanceMethodsStruct // go for methods
+typedef struct HexasoftO3ServiceInstanceApiStruct // go for methods
 {
-    void (* open)(char* file, CommaSepO3toIPT data);
-    void (* read)(char* here, CommaSepO3toIPT data);
-    int (* isAtEof)(CommaSepO3toIPT data);
-    void (* close)(CommaSepO3toIPT data);
+    void (* open)(char* file, CommaSepO3SIdataPT data);
+    void (* read)(char* here, CommaSepO3SIdataPT data);
+    int  (* isAtEof)(CommaSepO3SIdataPT data);
+    void (* close)(CommaSepO3SIdataPT data);
     
-}HexasoftO3goIT, *HexasoftO3goIPT;
+}HexasoftO3SIapiT, *HexasoftO3SIapiPT;
 
-extern HexasoftO3goIT HexasoftO3go;
+extern HexasoftO3SIapiT HexasoftO3SIapi;
 
-typedef struct HexasoftO3InstanceStruct // Having neither to nor go for complete instance
+typedef struct HexasoftO3ServiceInstanceStruct // Having neither to nor go for complete instance
 {
-    CommaSepO3toIT to;  // the data
-    HexasoftO3goIT go; // the methods
-}HexasoftO3IT, *HexasoftO3IPT;
+    CommaSepO3SIdataT data;  // the data
+    HexasoftO3SIapiT api; // the methods
+}HexasoftO3SIthisT, *HexasoftO3SIthisPT;
 
-#define HexasoftIT HexasoftO3IT
-#define HexasoftIPT HexasoftO3IPT
+#define HexasoftSIthisT HexasoftO3SIthisT
+#define HexasoftSIthisPT HexasoftO3SIPthisT
 
 #define newHexasoft newHexasoftO3
-extern HexasoftIT newHexasoft(gpTxPplGrpBatchTwoWayO0PT approvedParmName);
+extern HexasoftSIthisT newHexasoft(gpSllgChar64PT);
 
 /**
  * Class methods
@@ -48,14 +45,14 @@ extern HexasoftIT newHexasoft(gpTxPplGrpBatchTwoWayO0PT approvedParmName);
 typedef struct HexasoftO3ClassO3Struct
 {
     void (* noMethods)(void);
-}HexasoftO3CT, *HexasoftO3CPT;
+}HexasoftO3SCapiT, *HexasoftO3SCapiPT;
 
-#define HexasoftCT HexasoftO3CT
-#define HexasoftC HexasoftO3C
-extern HexasoftCT HexasoftC;
+#define HexasoftSCapiT HexasoftO3SCapiT
+#define HexasoftSCapi HexasoftO3SCapi
+extern HexasoftSCapiT HexasoftSCapi;
 
-#define initHexasofts initHexasoftsO3
-extern initStatusT initHexasofts(PermissionO0toIPT permissionP);
+//#define initHexasoft initHexasoftsO3
+//extern initStatusT initHexasofts(PermissionO0toIPT permissionP);
 
 /**
  * Some test constants.
@@ -65,4 +62,7 @@ extern initStatusT initHexasofts(PermissionO0toIPT permissionP);
 
 
 
-#endif /* defined(__HexasoftO3__) */
+#endif /* HexasoftO3_h */
+/**
+ *
+ */
--- HexasoftO3XCTests.m
+++ HexasoftO3XCTests.m
@@ -1,23 +1,27 @@
 //
 //  HexasoftO3Tests.m
+//  20200310Dev
 //
 //  Created by Robert R on 3/27/15.
-//  Copyright (c) 2015 Robert Russell Millward.
-//  All rights reserved.
+//  Copyright (c) 2015 Robert Russell Millward.  All rights reserved.
 //
-
-#import <XCTest/XCTest.h>
-#import "ArchitectureO0.h"
-#import "HexasoftO3.h"
-#import "PeopleGroupO0.h"
-#import "TestAidO0.h"
-#import "InitDestroyHelperO1.h"
-#import "SignatureFactoryO0.h"
-#import "CSVO0.h"
+// os
+#include <XCTest/XCTest.h>
+// helper and service api's
+//#import "PeopleGroupO0.h"
+//#import "InitDestroyHelperO1.h"
+//#import "SignatureFactoryO0.h"
+//#import "CSVO0.h"
+// data plans
+#include "ArchitectureZ0Plan.h"
+// other api's
+#include "HexasoftO3.h"
+// quality
+#import "TestAidZ0.h"
 
 #import "UnixStreamO0.h"
 
-@interface HexasoftO03Tests : XCTestCase
+@interface HexasoftO3Tests : XCTestCase
 
 @end
 
@@ -26,12 +30,12 @@
 static bool showFunctionName = false;
 
 - (void)setUp {
-    initSignatureFactoryO0(&TestAidO0QCre.infoTwoWay);
+    //initSignatureFactoryO0(&TestAidO0QCre.infoTwoWay);
 }
 
 - (void)tearDown {
     TestAidC.getCounts();
-    InitDestroyerHelperC.destroyAll(&TestAidO0QCre.infoTwoWay);
+    //InitDestroyerHelperC.destroyAll(&TestAidO0QCre.infoTwoWay);
 }
 
 
@@ -39,20 +43,23 @@ static bool showFunctionName = false;
 - (void)test2019ZdoHexasoft {
     if(TA0C.isScheduled(__FUNCTION__, showFunctionName))
     {
-        int rowCount = 0;
-        /*initStatusT status = */initHexasofts(&TestAidO0QCre.infoTwoWay);
-        HexasoftIT myHexasoft = newHexasoft(&TestAidO0QCre.infoTwoWay);
-        myHexasoft.open("IP2LOCATION-LITE-DB11", &myHexasoft);
-        
-        char here[2000];
-        while(!myHexasoft.isAtEof(&myHexasoft) )
-        {
-            myHexasoft.read(here, &myHexasoft);
-            rowCount++;
-        }
-        myHexasoft.close(&myHexasoft);
-        here[1999] = 0;
+//        int rowCount = 0;
+//        /*initStatusT status = */initHexasofts(&TestAidO0QCdata.infoTwoWay);
+//        HexasoftIT myHexasoft = newHexasoft(&TestAidO0QCdata.infoTwoWay);
+//        myHexasoft.open("IP2LOCATION-LITE-DB11", &myHexasoft);
+//        
+//        char here[2000];
+//        while(!myHexasoft.isAtEof(&myHexasoft) )
+//        {
+//            myHexasoft.read(here, &myHexasoft);
+//            rowCount++;
+//        }
+//        myHexasoft.close(&myHexasoft);
+//        here[1999] = 0;
     }
 }
 
 @end
+/**
+ *
+ */
